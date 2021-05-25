b42114e202624e53438c469f1314cd88058ff9bb
Aman Gupta <amang5840@gmail.com>
2021-05-25 16:53:29 +0530

    create sidebar

--- src/components/Sidebar/Sidebar.jsx
+++ src/components/Sidebar/Sidebar.jsx
@@ -1,12 +1,40 @@
 import React, { Component } from 'react';
+import {Group, RssFeed, Flag, PersonAdd, PostAdd, Description } from '@material-ui/icons';
 import './sidebar.css';
 
 class Sidebar extends Component {
     render() {
         return (
-            <aside className="sidebar">
-                Sidebar
-            </aside>
+          <aside className="sidebar">
+            <div className="sidebar-container">
+              <ul className="sidebar-list">
+                <li className="sidebar-list-item">
+                  <RssFeed className="sidebar-icon" />
+                  <span className="list-text">Feed</span>
+                </li>
+                <li className="sidebar-list-item">
+                  <PostAdd className="sidebar-icon" />
+                  <span className="list-text">Posts</span>
+                </li>
+                <li className="sidebar-list-item">
+                  <Description className="sidebar-icon" />
+                  <span className="list-text">Articles</span>
+                </li>
+                <li className="sidebar-list-item">
+                  <PersonAdd className="sidebar-icon" />
+                  <span className="list-text">Find Friends</span>
+                </li>
+                <li className="sidebar-list-item">
+                  <Group className="sidebar-icon" />
+                  <span className="list-text">Users</span>
+                </li>
+                <li className="sidebar-list-item">
+                  <Flag className="sidebar-icon" />
+                  <span className="list-text">Pages</span>
+                </li>
+              </ul>
+            </div>
+          </aside>
         );
     }
 }
