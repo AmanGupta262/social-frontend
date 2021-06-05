56a073a08d4af3e5db995e1ac972676d40516f92
Aman Gupta <amang5840@gmail.com>
2021-06-05 11:17:17 +0530

    user login 1

---
+++ src/reducers/auth.js
@@ -0,0 +1,37 @@
+import { LOGIN_START, LOGIN_SUCCESS, LOGIN_FAILED } from "../actions";
+
+const initialAuthState = {
+    user: {},
+    error: null,
+    isLoggedIn: false,
+    inProgress: false
+}
+
+export default function auth(state = initialAuthState, action) {
+    switch (action.type) {
+        case LOGIN_START:
+
+            return {
+                ...state,
+                inProgress: true
+            };
+        case LOGIN_SUCCESS:
+
+            return {
+                ...state,
+                user: action.user,
+                isLoggedIn: true,
+                inProgress: false,
+                error: null,
+            };
+        case LOGIN_FAILED:
+
+            return {
+                ...state,
+                inProgress: false
+            };
+
+        default:
+            return state;
+    }
+}
