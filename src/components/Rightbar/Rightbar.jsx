71849570f14983ccde8f28975e44727526bbb4d8
Aman Gupta <amang5840@gmail.com>
2021-06-27 00:18:05 +0530

    show all users

--- src/components/Rightbar/Rightbar.jsx
+++ src/components/Rightbar/Rightbar.jsx
@@ -1,10 +1,11 @@
-import React from 'react';
+import React from "react";
 import {} from "@material-ui/icons";
-import './rightbar.css';
-import { useSelector } from 'react-redux';
+import "./rightbar.css";
+import { useSelector } from "react-redux";
 
-function Rightbar({profile}) {
-  const {isLoggedIn, user} = useSelector(state => state.auth);
+function Rightbar({ profile }) {
+  const { isLoggedIn, user } = useSelector((state) => state.auth);
+  const { allUsers } = useSelector((state) => state.users);
   return (
     <aside className="rightbar">
       <div className="rightbar-container">
@@ -24,7 +25,7 @@ function Rightbar({profile}) {
                     <div className="remove-friend btn">Remove</div>
                   </div>
                 ))
-              )}             
+              )}
             </div>
           </div>
         )}
@@ -33,34 +34,22 @@ function Rightbar({profile}) {
           <div className="user-container">
             <h4 className="title">Users</h4>
             <div className="users">
-              <div className="user">
-                <div className="left">
-                  <img src="" alt="" className="profile-img" />
-                  <div className="name">Bob</div>
-                </div>
-                <div className="add-friend btn">Add Friend</div>
-              </div>
-              <div className="user">
-                <div className="left">
-                  <img src="" alt="" className="profile-img" />
-                  <div className="name">Bob</div>
-                </div>
-                <div className="add-friend btn">Add Friend</div>
-              </div>
-              <div className="user">
-                <div className="left">
-                  <img src="" alt="" className="profile-img" />
-                  <div className="name">Bob</div>
-                </div>
-                <div className="add-friend btn">Add Friend</div>
-              </div>
-              <div className="user">
-                <div className="left">
-                  <img src="" alt="" className="profile-img" />
-                  <div className="name">Bob</div>
-                </div>
-                <div className="add-friend btn">Add Friend</div>
-              </div>
+              {allUsers.length === 0 ? (
+                <h5>No Users to view</h5>
+              ) : (
+                allUsers.map(
+                  (singleUser) =>
+                    singleUser._id !== user._id && (
+                      <div className="user">
+                        <div className="left">
+                          <img src="" alt="" className="profile-img" />
+                          <div className="name">{singleUser.name}</div>
+                        </div>
+                        <div className="add-friend btn">Add Friend</div>
+                      </div>
+                    )
+                )
+              )}
             </div>
           </div>
         )}
